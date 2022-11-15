e585330115319e0828951d8058f189ff6f507b01
B-MNSN <manutsanan0@gmail.com>
2022-11-16 01:40:33 +0700

    add core

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('./config/database').connect();
 
 const express = require('express');
+var cors = require('cors');
 // const User = require('./model/user');
 // const bcrypt = require('bcryptjs');
 // const jwt = require('jsonwebtoken');
@@ -13,6 +14,7 @@ const login = require('./routes/login');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/register', register);
 app.use('/login', login);
