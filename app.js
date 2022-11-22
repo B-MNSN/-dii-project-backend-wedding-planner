d73dd5ba0927e5bbda824108a8920174dbf845cc
B-MNSN <manutsanan0@gmail.com>
2022-11-22 12:59:45 +0700

    update

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ const transaction = require('./routes/transactions');
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/register', register);
 app.use('/login', login);
