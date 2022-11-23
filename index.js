0b2ec294bc5f62e08b8f61ddda08aeca67be9109
B-MNSN <manutsanan0@gmail.com>
2022-11-23 12:59:19 +0700

    new

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require('http');
 const app = require('./app');
 const server  = http.createServer(app);
 
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
+const { PORT } = process.env;
+const port = process.env.PORT || PORT;
 
 //server listening
 server.listen(port, () => {
