9897ae9b0efa9d762b93c90fbce975ad05091051
B-MNSN <manutsanan0@gmail.com>
2022-11-23 15:13:09 +0700

    aygyg

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const http = require('http');
 const app = require('./app');
 const server  = http.createServer(app);
 
-const { PORT } = process.env;
+const { PORT } = process.env.PORT;
 const port = process.env.PORT || PORT;
 
 //server listening
-server.listen(port, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${port}`);
 });
